0a0ba5b98684d6c8f5ddd03eb2cedbe6342b9e2c
Ayan-farhad <khaanayan81@gmail.com>
2023-10-25 05:18:38 -0700

    assignment 1

---
+++ app.js
@@ -0,0 +1,159 @@
+
+            /**---Alert--- **/
+
+// Question 1
+
+// let name = prompt('Enter your name')
+// alert(name+ ' ' +'Welcome to Jvascript'
+
+
+// Question 2
+
+// alert('Error! apna correct password dalo')
+
+
+// Question 3
+
+//   alert('line break \nsuccessfuly linebreak')
+
+
+// Question 4
+
+// alert('Web and mobile Developer ')
+// alert('full stack \nmern stack ')
+
+
+// Question 5
+
+// console.log('alert("browsers developer console")')
+// alert("browsers developer console")
+
+
+
+
+ //**--- Variable for strings---**/
+
+
+// Qeustion 1
+
+// let username = "Ayan Farhad"
+// console.log(username);
+
+
+// Question 2
+
+// let myName = "Ayan Farhad"
+// console.log(myName);
+
+
+
+// Question 3
+
+// let titledMessage = "Hello World"
+// alert(titledMessage);
+
+
+
+// Question 4
+
+// let bio = 'Students biodata'
+// alert(bio);
+  
+// let nam = 'Ayan Farhad'
+// alert(nam)
+
+// let age = 17
+// alert(age )
+
+// let course = "Web And Mobie developer"
+// alert(course);
+
+
+
+// Question 5
+
+// let variable = "Hello JS"
+// alert(variable + '\n'+ 'JS')
+
+
+
+// Question 6
+
+// let selfName = 'Ayan Farhad'
+// let numberOfChildren = '0' 
+// let gLocation = 'pakistan'
+// let jobTitle = 'Student'
+// alert('My name is ' + selfName + ' I have ' + numberOfChildren + ' kids live in ' + gLocation + ' and iam a ' +jobTitle);
+
+
+
+// Question 7
+
+// let Email = 'khaanayan81@gmail.com'
+// let emailAddress = "My Email Address is " + Email
+// alert(emailAddress);
+
+
+
+// Question 8
+
+// let book = 'A smart way to learn JavaScript'
+// alert('I am learning JS from ' + book)
+
+
+
+// Question 9
+
+// let nam = 'Ayan Farhad'
+// let age = 17
+// let institute = 'xpertizo university'
+// let course = 'Mobile & Cloud Computing - Module A'
+// let script = 'My name is ' + nam + ' I am ' + age + ' years old. I study in ' + institute + ' and I enrolled in ' + course  
+// alert(script);
+
+
+
+// Question 10
+
+// document.writeln('This is last question of variable for string through js');
+
+
+
+
+
+
+/**---Variable for number ---**/
+
+
+
+// Question 1 
+
+// let age = 17
+// alert(age)
+
+
+
+
+// Question 2
+
+// let times = 1
+// times = ' You visited this site ' + times + ' times '
+// alert(times)
+
+
+// Question 3
+
+
+// let birthYear = 2006
+// birthYear = ' My birth year is ' + birthYear
+// document.write(birthYear)
+
+
+// Question 4 
+
+ 
+// let Name = 'hamza'
+// let quantity = 7
+// let product= 'Cargo(L)'
+// let clothingStore = Name + ' order ' + quantity + ' ' + product + ' abc Clothing store'
+// document.write(clothingStore)
