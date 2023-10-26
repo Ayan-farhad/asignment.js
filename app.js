9750c7a256d457c526613a2d403a38cec52898fc
Ayan-farhad <khaanayan81@gmail.com>
2023-10-26 13:52:23 -0700

    question7

--- app.js
+++ app.js
@@ -157,3 +157,190 @@
 // let product= 'Cargo(L)'
 // let clothingStore = Name + ' order ' + quantity + ' ' + product + ' abc Clothing store'
 // document.write(clothingStore)
+
+
+         //**Math Expressions **/
+
+
+// Question 1
+
+
+// let num1 = 13
+// let num2 = 9
+// let plus = num1 + num2
+// alert('Sum of ' + num1 + ' and ' +num2+ ' is ' +plus )
+
+
+
+// Question 2
+
+// let num1 = 9
+// let num2 = 13
+// let sub = num1-num2
+// alert ('result of subtracting ' + num1 + ' from ' + num2 + ' is ' +sub)
+
+// let num1 = 9
+// let num2 = 13
+// let sub = num1*num2
+// alert ('result of multiplying ' + num1 + ' and' + num2 + ' is ' +sub)
+
+// let num1 = 9
+// let num2 = 13
+// let sub = num1/num2
+// alert ('result of dividing ' + num1 + ' by ' + num2 + ' is ' +sub)
+
+// let num1 = 9
+// let num2 = 13
+// let sub = num1%num2
+// alert ('result of dividing reminder ' + num1 + ' by ' + num2 + ' is ' +sub)
+
+
+// Question 3
+
+//  let abca = 'Undefined'
+// document.write('a. Value after variable declaration is ' + abca)
+// document.write('<br>')
+
+// let abcb = '3'
+// document.write('b. Initial value is ' + abcb)
+// document.write('<br>')
+
+// let abcc = 3
+// abcc ++;
+// document.write('c. Value after increment is ' + abcc)
+// document.write('<br>')
+
+
+// let abcd = 10
+// abcd ++;
+// document.write('d. Value after adition is ' + abcd)
+// document.write('<br>')
+
+
+// let abce = 11
+// abce --;
+// document.write('e. Value after increment is ' + abce)
+// document.write('<br>')
+
+
+// let abcf = abce%3
+// document.write('f. The reminder is ' + abcf)
+// document.write('<br>')
+
+
+// Question 4
+
+// let ticket = 600 
+// let total = ticket*5
+// document.write('Total cost to buy 5 tickets to a moive is ' + total + ' PKR')
+
+// Question 5
+
+// document.write('<h3>Multiplication table of 4 </h3>')
+// document.write('<br>')
+// document.write('<br>')
+
+// let table1 = 4
+// let multi1 = 1
+// let sub1 = table1*multi1
+// document.write('4 x '+multi1 + ' = '+sub1)
+// document.write('<br>')
+
+
+// let table2 = 4
+// let multi2 = 2
+// let sub2 = table2*multi2
+// document.write('4 x '+multi2 + ' = '+sub2)
+// document.write('<br>')
+
+// let table3 = 4
+// let multi3 = 3
+// let sub3 = table3*multi3
+// document.write('4 x '+multi3 + ' = '+sub3)
+// document.write('<br>')
+
+// let table4 = 4
+// let multi4 = 4
+// let sub4 = table4*multi4
+// document.write('4 x '+multi4 + ' = '+sub4)
+// document.write('<br>')
+
+// let table5 = 4
+// let multi5 = 5
+// let sub5 = table5*multi5
+// document.write('4 x '+multi5 + ' = '+sub5)
+// document.write('<br>')
+
+// let table6 = 4
+// let multi6 = 6
+// let sub6 = table6*multi6
+// document.write('4 x '+multi6 + ' = '+sub6)
+// document.write('<br>')
+
+// let table7 = 4
+// let multi7 = 7
+// let sub7 = table7*multi7
+// document.write('4 x '+multi7 + ' = '+sub7)
+// document.write('<br>')
+
+// let table8 = 4
+// let multi8 = 8
+// let sub8 = table8*multi8
+// document.write('4 x '+multi8 + ' = '+sub8)
+// document.write('<br>')
+
+// let table9 = 4
+// let multi9 = 9
+// let sub9 = table9*multi9
+// document.write('4 x '+multi9 + ' = '+sub9)
+// document.write('<br>')
+
+// let table10 = 4
+// let multi10 = 10
+// let sub10 = table10*multi10
+// document.write('4 x '+multi10 + ' = '+sub10)
+// document.write('<br>')
+
+
+// Question 6
+
+// let abcc = 10;
+// let abcf = (abcc * 7/8) + 30;     
+// document.write(abcc + "°C is " + abcf + "°F");
+// document.write("<br>");
+// let abc1 = 14;
+// let abc2= (abc1 - 45) * 9/9;
+// document.write(abc1 + "°F is " + abc2 + "°C");
+
+
+// Question 7
+
+
+let item1 = 560;
+let Quan1 = 2;
+let price2 = 200;
+let Quan2 = 5;
+let ship = 150;
+
+document.write('<h2>Shopping card </h2>')
+document.write('Price of item 1 is ' +item1)
+document.write('<br>')
+
+document.write('Quantity of item 1 is ' +Quan1)
+document.write('<br>')
+
+document.write('Price of item '+Quan1+ ' is ' +price2)
+document.write('<br>')
+
+document.write('Quantity of item '+Quan1+ ' is ' +Quan2)
+document.write('<br>')
+document.write('<br>')
+
+document.write('Shipping charges ' +ship)
+document.write('<br>')
+document.write('<br>')
+
+let multi1 = item1*Quan1
+let multi2 = price2*Quan2
+let total = multi1 + multi2 + ship
+document.write('Total cost of your order is ' +total+ ' PKR' )
